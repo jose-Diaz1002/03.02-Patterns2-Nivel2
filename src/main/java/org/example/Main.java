2f743ed9b02e54e90718124d9e782b3dec66ea25
JoseDiaz <jldi1002@hotmail.com>
2025-06-05 23:44:35 +0200

    de master a main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         StockBrokerAgent agent = new StockBrokerAgent();
 
-        BrokerAgency agency1 = new ConcreteBrokerAgency("Alpha Finance");
-        BrokerAgency agency2 = new ConcreteBrokerAgency("Beta Investments");
-        BrokerAgency agency3 = new ConcreteBrokerAgency("Omega Capital");
+        BrokerAgency agency1 = new ConcreteBrokerAgency("Avalon");
+        BrokerAgency agency2 = new ConcreteBrokerAgency("Skyline");
+        BrokerAgency agency3 = new ConcreteBrokerAgency("Falcon");
 
         agent.addObserver(agency1);
         agent.addObserver(agency2);
